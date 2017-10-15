4eb3b51dd079c121152b4eee9335f02c61391440
fmaylinch <ferranmaylinch@gmail.com>
2017-10-15 22:05:17 +0200

    Convert App into a class with state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import VideoList from './components/VideoList'
 import youtubeApiSample from './youtube-api-sample.json'
 
-const App = () => {
+class App extends Component {
 
-  const videos = youtubeApiSample.items
-    .filter(v => v.id.kind === "youtube#video")
-    .map(v => ({id: v.id.videoId, title: v.snippet.title}))
+  constructor(props) {
+    super(props)
 
-  return (
-    <VideoList videos={videos}></VideoList>
-  )
+    const videos = youtubeApiSample.items
+      .filter(v => v.id.kind === "youtube#video")
+      .map(v => ({id: v.id.videoId, title: v.snippet.title}))
+
+    this.state = { videos: videos }
+  }
+
+  render() {
+
+    return (
+      <VideoList videos={this.state.videos}></VideoList>
+    )
+  }
 }
 
 export default App
