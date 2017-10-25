9bd2c20cae2c4b6eaef86a655297f2668d05609d
fmaylinch <ferranmaylinch@gmail.com>
2017-10-25 13:35:18 +0200

    Add test for VideoUtil

--- src/util/YoutubeUtil.test.js
+++ src/util/YoutubeUtil.test.js
@@ -1,7 +1,25 @@
 
+import YoutubeUtil from './YoutubeUtil'
+import data from './api-search-sample.json'
+
 describe('YoutubeUtil', () => {
 
-  it('runs tests correctly', () => {
-    expect(1+1).toEqual(2)
+  it('extracts videos correctly', () => {
+
+    const videos = YoutubeUtil.extractVideos(data)
+
+    expect(videos.length).toEqual(9)
+
+    const expectedFirstVideo = {
+      id: "4TPqUvy1vYU",
+      image: {
+        width: 320,
+        height: 180,
+        url: "https://i.ytimg.com/vi/4TPqUvy1vYU/mqdefault.jpg"
+      },
+      title: "St. Vincent - New York (Official Video)"
+    }
+
+    expect(videos[0]).toEqual(expectedFirstVideo)
   })
 })
